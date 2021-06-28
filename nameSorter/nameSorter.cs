23cc86e4835dc03864a3a6aec8c3a2ef50dade63
darpan30 <darpanhdhruva15@gmail.com>
2021-06-28 14:33:21 +1000

    Add project files.

---
+++ nameSorter/nameSorter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace nameSorter
+{
+    public class nameSorter
+    {
+        //This method will return a sorted names in array
+        public static string[] sortNames(string fileName)
+        {
+            // read the .txt file with names
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+
+            //create a list of all names
+            var listNames = new List<string>();
+
+            // Display the file contents by using a foreach loop.
+            foreach (string line in lines)
+            {
+                int a = 0;
+                int myWord = 1;
+                //get number of words in each name
+                while (a <= line.Length - 1)
+                {
+                    if (line[a] == ' ' || line[a] == '\n' || line[a] == '\t')
+                    {
+                        myWord++;
+                    }
+                    a++;
+                }
+
+                // A name must have at least 1 given name and may have upto 3 given names
+                if (myWord >= 2 && myWord < 5)
+                {
+                    listNames.Add(line);
+                }
+
+            }
+
+            //sort the list of names according to first name 
+            listNames.Sort();
+
+            //sort the list of names according to last name
+            IEnumerable<string> sortedNames = from person in listNames.Cast<string>()
+                                              let lastName = person.Split(' ').Last()
+                                              orderby lastName ascending
+                                              select person;
+            return sortedNames.ToArray();
+        }
+
+        //Main Method
+        public static void Main(string[] args)
+        {
+            // Pass the unsorted file path from the sortNames method
+            try
+            {
+                var sortedNames = sortNames(@"C:\\Users\\DHRUVA\\Desktop\\nameSorter\\nameSorter\\unsorted-names-list.txt");
+                //Pass the file path and file name to the StreamReader constructor
+                StreamWriter sw = new StreamWriter(@"C:\\Users\\DHRUVA\\Desktop\\nameSorter\\nameSorter\\sorted-names-list.txt");
+
+
+                // write the sorted names in a .txt file and print sorted list of names to screen
+                foreach (var name in sortedNames)
+                {
+                    if (name != null)
+                    {
+                        sw.WriteLine(name);
+                        Console.WriteLine(name);
+                    }
+                }
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }   
+            Console.WriteLine("\n\nPress Any key to exit the progran");
+            Console.ReadKey();
+        }   
+    }
+}
+
+
---
+++ nameSorterTests/nameSorterTests.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace nameSorter.Tests
+{
+    [TestClass()]
+    public class nameSorterTests
+    {
+        //test method for provided unsorted-names-list.txt file
+        [TestMethod()]
+        public void providedFileTest()
+        {
+            //create an array of expected output for provided unsorted-names-list.txt file
+            string[] expectedOutput = { "Marin Alvarez",
+                                       "Adonis Julius Archer",
+                                       "Beau Tristan Bentley",  
+                                       "Hunter Uriah Mathew Clarke",
+                                       "Leo Gardner",
+                                       "Vaughn Lewis",
+                                       "London Lindsey",
+                                       "Mikayla Lopez",
+                                       "Janet Parsons",
+                                       "Frankie Conner Ritter",
+                                       "Shelby Nathan Yoder"
+            };
+
+            //actual output after sorting names for provided unsorted-names-list.txt file
+            string[] actualOutput = nameSorter.sortNames(@"C:\\Users\\DHRUVA\\Desktop\\nameSorter\\nameSorter\\testFiles\\unsorted-names-list.txt");
+            //check weather expeted output and actual output is equal or not
+            CollectionAssert.AreEqual(expectedOutput,actualOutput);
+        }
+
+        //test method for .txt file with same last name
+        [TestMethod()]
+        public void sameLastNameTest()
+        {
+            //create an array of expected output for provided samelastname.txt file
+            string[] expectedOutput = { "Alok Sudhirbhai Dhruva",
+                                       "Ankur Dipakbhai Dhruva",
+                                       "Darpan Hirenbhai Dhruva",
+                                       "Milap Kaushikbhai Dhruva",
+                                       "Samkit Rajendrabhai Dhruva"
+            };
+
+            //actual output after sorting names for provided samelastname.txt file
+            string[] actualOutput = nameSorter.sortNames(@"C:\\Users\\DHRUVA\\Desktop\\nameSorter\\nameSorter\\testFiles\\samelastname.txt");
+            //check weather expeted output and actual output is equal or not
+            CollectionAssert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        //test method for .txt file with same first name
+        [TestMethod()]
+        public void sameFirstNameTest()
+        {
+            //create an array of expected output for provided sameFirstName.txt file
+            string[] expectedOutput = { "Darshit Anilbhai Dhruva",
+                                       "Darshit Mansukhbhai Dhruva",
+                                       "Darshit Shirishkumar Jasani",
+                                       "Darshit Atulbhai Parekh"
+            };
+
+            //actual output after sorting names for provided sameFirstName.txt file
+            string[] actualOutput = nameSorter.sortNames(@"C:\\Users\\DHRUVA\\Desktop\\nameSorter\\nameSorter\\testFiles\\sameFirstName.txt");
+            //check weather expeted output and actual output is equal or not
+            CollectionAssert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        //test method for .txt file with same last name and same given name
+        [TestMethod()]
+        public void sameLastGivenNameTest()
+        {
+            //create an array of expected output for provided sameLastGivenName.txt file
+            string[] expectedOutput = { "Alok Sudhirbhai Dhruva",
+                                       "Ankur Dipakbhai Dhruva",
+                                       "Darpan Atulbhai Dhruva",
+                                       "Darpan Hirenbhai Dhruva",
+                                       "Darpan Rajendrabhai Dhruva",
+                                       "Milap Kaushikbhai Dhruva",
+                                       "Samkit Rajendrabhai Dhruva"
+            };
+
+            //actual output after sorting names for provided sameLastGivenName.txt file
+            string[] actualOutput = nameSorter.sortNames(@"C:\\Users\\DHRUVA\\Desktop\\nameSorter\\nameSorter\\testFiles\\sameLastGivenName.txt");
+            //check weather expeted output and actual output is equal or not
+            CollectionAssert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        //test method for must have at least 1 given name and may have upto 3 given names
+        [TestMethod()]
+        public void validNameTest()
+        {
+            //create an array of expected output for provided validName.txt file
+            string[] expectedOutput = {"Keyur Giteshkumar Dave",
+                                       "Alok Sudhirbhai Dhruva",
+                                       "Ankur Dipakbhai Dhruva",
+                                       "Darpan Atulbhai Dhruva",
+                                       "Darpan Hirenbhai Dhruva",
+                                       "Darpan Rajendrabhai Dhruva",
+                                       "Milap Kaushikbhai Dhruva",
+                                       "Samkit Rajendrabhai Dhruva"
+            };
+
+            //actual output after sorting names for provided validName.txt file
+            string[] actualOutput = nameSorter.sortNames(@"C:\\Users\\DHRUVA\\Desktop\\nameSorter\\nameSorter\\testFiles\\validName.txt");
+            //check weather expeted output and actual output is equal or not
+            CollectionAssert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
